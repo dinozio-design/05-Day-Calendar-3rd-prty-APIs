47a870ac1ba602cd83f79a389c3025f96e3d49a7
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-05-23 16:07:55 -0400

    fixing links as we go

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -2,6 +2,14 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+ //DOM element references
+ var timeDisplayEl = $('#currentDay');
+  // handle displaying the time
+  function displayTime() {
+    var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,6 +17,21 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
+  // ```
+  // GIVEN I am using a daily planner to create a schedule
+
+  // WHEN I scroll down
+  // THEN I am presented with timeblocks for standard business hours
+  // WHEN I view the timeblocks for that day
+  // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+  // WHEN I click into a timeblock
+  // THEN I can enter an event
+  // WHEN I click the save button for that timeblock
+  // THEN the text for that event is saved in local storage
+  // WHEN I refresh the page
+  // THEN the saved events persist
+  // ```
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
